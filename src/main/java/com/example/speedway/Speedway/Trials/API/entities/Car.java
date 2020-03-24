4d2856d1449a75b1725c8e3fac8a656c6acbc2ac
Zika Chea <jallsadou@gmail.com>
2020-03-23 20:29:20 -0500

    added entity

---
+++ src/main/java/com/example/speedway/Speedway/Trials/API/entities/Car.java
@@ -0,0 +1,103 @@
+package com.example.speedway.Speedway.Trials.API.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Car {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name= "nickname")
+    private String nickname;
+ 
+    private String model;
+
+    private LocalDateTime year;
+
+    private String driver;
+
+    private String status;
+    private int top_speed;
+
+
+    public Car(Long id, String nickname, String model, LocalDateTime year, String driver, String status, int top_speed) {
+        this.id = id;
+        this.nickname = nickname;
+        this.model = model;
+        this.year = year;
+        this.driver = driver;
+        this.status = status;
+        this.top_speed = top_speed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public LocalDateTime getYear() {
+        return year;
+    }
+
+    public void setYear(LocalDateTime year) {
+        this.year = year;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getTop_speed() {
+        return top_speed;
+    }
+
+    public void setTop_speed(int top_speed) {
+        this.top_speed = top_speed;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", nickname='" + nickname + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", driver='" + driver + '\'' +
+                ", status='" + status + '\'' +
+                ", top_speed=" + top_speed +
+                '}';
+    }
+}
