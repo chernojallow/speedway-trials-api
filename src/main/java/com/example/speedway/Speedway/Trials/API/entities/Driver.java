f0f1515904bff16a8311b2f02e1c0406faad9e7c
andrewtkemp@gmail.com <andrewtkemp@gmail.com>
2020-03-23 20:29:54 -0500

    Driver Entity

---
+++ src/main/java/com/example/speedway/Speedway/Trials/API/entities/Driver.java
@@ -0,0 +1,40 @@
+package com.example.speedway.Speedway.Trials.API.entities;
+
+import jdk.vm.ci.meta.Local;
+
+import java.time.LocalDate;
+
+public class Driver {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private Integer age;
+    private LocalDate birthDate;
+    private Car car;
+    private Integer wins;
+    private Integer losses;
+//    private RaceRecord raceRecord;
+//    private Sponsor sponsor;
+    public Driver(){}
+    // New Driver
+    public Driver(String firstName, String lastName, String nickName, Integer age, LocalDate birthDate, Car car) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.birthDate = birthDate;
+        this.car = car;
+        this.wins = 0;
+        this.losses = 0;
+    }
+    // Adding in Driver Information Manually
+    public Driver(String firstName, String lastName, String nickName, Integer age, LocalDate birthDate, Car car, Integer wins, Integer losses) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.birthDate = birthDate;
+        this.car = car;
+        this.wins = wins;
+        this.losses = losses;
+    }
+}
