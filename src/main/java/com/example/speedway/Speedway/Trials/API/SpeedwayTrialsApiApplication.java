7a7324a1cc48a3cdd872321e236b2395de59efc3
andrewtkemp@gmail.com <andrewtkemp@gmail.com>
2020-03-23 19:56:01 -0500

    initial commit

---
+++ src/main/java/com/example/speedway/Speedway/Trials/API/SpeedwayTrialsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.speedway.Speedway.Trials.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedwayTrialsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedwayTrialsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/speedway/Speedway/Trials/API/SpeedwayTrialsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speedway.Speedway.Trials.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedwayTrialsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
